04896d7c5e12e90a68a100b9f408cac73ded59a0
Natalia Zaitseva <zaitsevn@oregonstate.edu>
2025-04-05 16:08:50 -0700

    Flashcard start

--- pages/deckManager.tsx
+++ pages/deckManager.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import { Link } from 'react-router-dom';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
@@ -13,8 +14,25 @@ const Header: React.FC = () => {
         // Add your logout logic here
     };
 
+=======
+import styles from './Flashcard.module.css';
+import { useState } from 'react';
+import Flashcard from '../static/components/Flashcard'
+import Image from 'next/image';
+
+{/*make it to load array from JSON*/}
+var testTuple: [string, string][];
+testTuple = [["Hey", "Steve"], ["Second", "Bill"], ["Third", "Jeff"]];
+
+{/*deck name will be a name property from JSON file*/}
+
+function handleClick() {
+    console.log("increment like count")
+  }
+>>>>>>> Stashed changes
 
     return (
+<<<<<<< Updated upstream
         <header style={{ backgroundColor: '#282c34', padding: '20px', color: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <h1>Deck Manager</h1>
 
@@ -25,6 +43,32 @@ const Header: React.FC = () => {
             </Link>
 
         </header>
+=======
+        <div style={{ padding: '2rem', textAlign: 'center' }}>
+            <h1>Deck Manager</h1>
+            <p>Welcome to the Deck Manager page. Use this page to manage your decks.</p>
+            <h1>Deck Name</h1>
+            <button onClick={handleClick}>
+            <Image
+                src="/images/left-arrow.png"
+                height={80}
+                width={80}
+                alt="arrow"
+            />
+            </button>
+            {testTuple.map(([front, back], index) => (
+                <Flashcard key={index} front={front} back={back} />
+            ))}
+            <button onClick={handleClick}>
+            <Image
+                src="/images/right-arrow.png"
+                height={80}
+                width={80}
+                alt="arrow"
+            />
+            </button>
+        </div>
+>>>>>>> Stashed changes
     );
 };
 
